f514f7a1e0c770ad0699116cea0db0b079338387
Charlotte Weager <senwyn@googlemail.com>
2021-07-12 23:33:47 +0100

    Updated styling

--- src/containers/message.jsx
+++ src/containers/message.jsx
@@ -3,11 +3,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Message extends Component {
+
+  convertDate = () => {
+    const date = new Date(this.props.message.created_at);
+    return (`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`);
+  }
+
   render() {
     console.log(this.props.userColor);
     return (
       <div className="message-style">
-        <h3 style={{ color: `${this.props.userColor}` }}>{this.props.message.author}</h3><h5>{this.props.message.created_at}</h5>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <h3 style={{ color: `${this.props.userColor}`, display: "inline-block" }}>{this.props.message.author}</h3>
+          <h5 style={{ display: "inline-block" }}>{this.convertDate()}</h5>
+        </div>
         <p>{this.props.message.content}</p>
       </div>
     );
